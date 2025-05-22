266252089bba59f13577d4a3053083a087eaed95
CrazyGoat <145066947+lacabraloca1@users.noreply.github.com>
2025-05-21 20:18:33 -0600

    Se crea una función en archivo de utils para poder realizar funciones y luego esta se importa para poder realizar las pruebas con diferentes tipos de datos para poder ver que su funcionamiento es correcto.

---
+++ Practica3/main.js
@@ -0,0 +1,9 @@
+const restar = require('./utils').restar;
+
+console.log(restar(10, 5)); 
+console.log(restar(3, 7)); 
+console.log(restar(8, 0)); 
+console.log(restar(-4, -2));
+console.log(restar(5, 10));
+console.log(restar(100, 50));
+console.log(restar(20, 30));
