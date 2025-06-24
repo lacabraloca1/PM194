016291cbc1d142ed0b362c97da86dc1c819419f5
CrazyGoat <145066947+lacabraloca1@users.noreply.github.com>
2025-06-24 16:24:09 -0600

    Creacion y modificacion de splash screen

--- my-app/App.js
+++ my-app/App.js
@@ -1,75 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button ,TextInput , Alert , ScrollView } from 'react-native';
-import React, { useState } from 'react';
+import * as SpalshScreen from 'expo-splash-screen';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+SpalshScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [nombre , setNombre] = useState('');
-  const [password , setPassword] = useState('');
-  const [comments, setComments] = useState('');
-  const [showComments, setShowComments] = useState('')
-  const [Age , setAge] = useState('');
+  const [appRead,setAppReady] = useState(false);
 
-  const showAlert = () => {
-    if (nombre.trim() === '' || password.trim() === '' || Age.trim() === '') {
-      window.alert('Por favor, completa todos los campos.');
-    }
-    else {
-      window.alert(
-        `Nombre: ${nombre}\nContraseña: ${password}\nEdad: ${Age}\nComentarios: ${comments}`
-      );
-    }
-  }
-
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>Nombre (normal):</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Escribe tu nombre"
-        value={nombre}
-        onChangeText={setNombre}
-      />
-
-      <Text style={styles.title}>Contraseña (oculta):</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Escribe tu contraseña"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry={true}
-      />
-
-      <Text style={styles.title}>Edad:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Escribe tu edad"
-        value={Age}
-        onChangeText={setAge}
-        keyboardType="numeric"
-      />
-
-      <Text style={styles.title}>Comentarios Multilinea:</Text>
-      <TextInput
-        style={[styles.input, { height: 100, textAlignVertical: 'top' }]}
-        placeholder="Escribe tus comentarios"
-        value={comments}
-        onChangeText={setComments}
-        multiline={true}
-        numberOfLines={4}
-      />
-
-      <Text style={styles.title}>Campo solo lectura:</Text>
-      <TextInput
-        style={styles.input}
-        value="Este campo es de solo lectura"
-        editable={false}
-      />
-
-      <Button title="Mostrar alerta" onPress={showAlert} />
-    </ScrollView> 
-  );
+  useEffect(() => {
+    setTimeout(() => {
+      setAppReady(true);
+      SpalshScreen.hideAsync();
+    },2000);
+  }, []);
 }
-
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
